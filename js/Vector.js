9224b1ad65d168be8596ced9969e7d8b5fd7542b
lukesrw <lukesrw+github@gmail.com>
2021-06-28 16:19:02 +0100

    Added distanceTo method, fromClosest constructor

--- js/Vector.js
+++ js/Vector.js
@@ -25,7 +25,7 @@ function Vector(x, y, z) {
     /**
      * Return normalized form of Vector
      *
-     * @returns {Vector} normalized vector
+     * @returns { Vector } normalized vector
      */
     that.normalize = function () {
         var magnitude = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
@@ -39,9 +39,22 @@ function Vector(x, y, z) {
         );
     };
 
-    that.x = x || 0;
-    that.y = y || 0;
-    that.z = z || 0;
+    /**
+     * Calculate distance to another vector
+     *
+     * @param { { x?: number, y?: number, z?: number } | Vector } vector to measure
+     * @returns { number } distance between vectors
+     */
+    that.distanceTo = function (vector) {
+        return Math.sqrt(
+            Math.pow(Math.abs(that.x - vector.x), 2) +
+                Math.pow(Math.abs(that.y - vector.y), 2)
+        );
+    };
+
+    that.x = x ? parseFloat(x) : 0;
+    that.y = y ? parseFloat(y) : 0;
+    that.z = z ? parseFloat(z) : 0;
 }
 
 /**
@@ -55,4 +68,27 @@ Vector.fromPerpendicular = function (vector1, vector2) {
         -(vector2.y - vector1.y),
         vector2.x - vector1.x
     ).normalize();
+};
+
+Vector.fromClosest = function (point, vertices) {
+    var closest = {
+        vertex: null,
+        distance: Infinity,
+        delta: null
+    };
+
+    var distance;
+    vertices.forEach(function (vertex) {
+        distance = vertex.distanceTo(point);
+        if (distance < closest.distance) {
+            closest.distance = distance;
+            closest.vertex = vertex;
+            closest.delta = {
+                x: vertex.x - point.x,
+                y: vertex.y - point.y
+            };
+        }
+    });
+
+    return new Vector(closest.delta.x, closest.delta.y).normalize();
 };
