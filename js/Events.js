30e45b9b0f3090e6742770ac18f5f9843c3adb7b
lukesrw <lukesrw+github@gmail.com>
2021-07-02 21:55:16 +0100

    Split event methods into Events prototype

---
+++ js/Events.js
@@ -0,0 +1,53 @@
+function Events() {}
+
+/**
+ * Start listening for events emitted
+ *
+ * @param { string } event to listen for
+ * @param { function } callback to run on event
+ * @returns { this } current object
+ */
+Events.prototype.addEventListener = function (event, callback) {
+    if (event in this.events) this.events[event].push(callback);
+
+    return this;
+};
+
+/**
+ * Stop listening for events emitted
+ *
+ * @param { string } event to stop listening for
+ * @param { function } callback to stop running on event
+ * @returns { this } current object
+ */
+Events.prototype.removeEventListener = function (event, callback) {
+    if (event in this.events) {
+        this.events[event] = this.events[event].filter(function (listener) {
+            return callback !== listener;
+        });
+    }
+
+    return this;
+};
+
+/**
+ * Emit event to listeners
+ *
+ * @param { string } event to emit
+ * @param { object } [detail] to attach to event
+ * @returns { this } current object
+ */
+Events.prototype.emit = function (event, detail) {
+    if (event in this.events) {
+        this.events[event].forEach(function (listener) {
+            listener(
+                new CustomEvent(event, {
+                    detail: detail || {}
+                }),
+                this
+            );
+        });
+    }
+
+    return this;
+};
